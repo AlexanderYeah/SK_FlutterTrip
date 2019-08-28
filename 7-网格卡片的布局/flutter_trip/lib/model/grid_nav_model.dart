9c7a85625099744deb0e4001812854a0f4c69a6d
AY <yeshengke@163.com>
2019-08-28 17:49:02 +0800

    网格卡片的布局

---
+++ 7-网格卡片的布局/flutter_trip/lib/model/grid_nav_model.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter_trip/model/common_model.dart';
+
+
+//  hotel | Object	| NonNull
+// flight | Object	|	NonNull
+// travel | Object	|	NonNull
+
+class GridNavModel
+{
+   final GridNavItem hotel;
+   final GridNavItem flight;
+   final GridNavItem travel;
+
+   // 构造方法
+   GridNavModel({this.hotel,this.flight,this.travel});
+
+   factory  GridNavModel.fromJson(Map<String,dynamic> json){     
+     return json != null?
+     GridNavModel(
+       hotel: GridNavItem.fromJson(json['hotel']),
+       flight: GridNavItem.fromJson(json['flight']),
+       travel: GridNavItem.fromJson(json['travel']),
+     ):null;
+    
+   }
+}
+
+class GridNavItem {
+
+    final String startColor;
+    final String endColor;
+    final CommonModel mainItem;
+    final CommonModel item1;
+    final CommonModel item2;
+    final CommonModel item3;
+    final CommonModel item4;
+
+    // 构造方法
+    GridNavItem({this.startColor,this.endColor,this.mainItem,this.item1,this.item2,this.item3,this.item4});
+
+    factory  GridNavItem.fromJson(Map<String,dynamic> json){     
+      return GridNavItem(
+        startColor: json['startColor'],
+        endColor: json['endColor'],
+        mainItem:CommonModel.fromJson(json['mainItem']),
+        item1: CommonModel.fromJson(json['item1']),
+        item2: CommonModel.fromJson(json['item2']),
+        item3: CommonModel.fromJson(json['item3']),
+        item4: CommonModel.fromJson(json['item4']),
+        );
+    }
+
+}
---
+++ 7-网格卡片的布局/flutter_trip/lib/pages/search_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class SearchPage extends StatefulWidget
+{
+    // 重写Create State 方法
+    @override
+    _SearchPageState createState() => _SearchPageState();
+
+}
+
+
+class _SearchPageState extends State<SearchPage>
+{
+
+ 
+    @override  
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Center(
+          child: Text("搜索"),
+        )
+      );
+    }
+
+}
